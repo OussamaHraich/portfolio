a1bd03fa4ed74ab16bb73563663cc14245ae9467
OussamaHraich <50490684+OussamaHraich@users.noreply.github.com>
2019-06-01 16:01:22 +0000

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html class="no-js">
+  <head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <link rel="shortcut icon" href="img/logon.png" />
+    <meta charset="UTF-8" />
+    <title>Hraich Oussama</title>
+
+    <link
+      href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700"
+      rel="stylesheet"
+    />
+    <link
+      href="https://fonts.googleapis.com/css?family=Archivo+Narrow"
+      rel="stylesheet"
+    />
+    <!--
+			CSS
+			============================================= -->
+    <link
+      rel="stylesheet"
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css" />
+    <link rel="stylesheet" href="../css/contact.css" />
+  </head>
+
+  <body>
+    <div id="universe">
+      <div class="protfolio-wrap">
+        <!-- Start Header Area -->
+        <header class="default-header">
+          <nav class="navbar navbar-expand-lg  navbar-light">
+            <div class="container">
+              <a class="navbar-brand" href="index.html">
+                <img src="../img/logo.png" />
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <i class="fa fa-bars" aria-hidden="true"></i>
+              </button>
+
+              <div
+                class="collapse navbar-collapse justify-content-end align-items-center"
+                id="navbarSupportedContent"
+              >
+                <ul class="navbar-nav">
+                  <li><a href="../#home">Acceuil</a></li>
+                  <li><a href="../travail">Portfiolio</a></li>
+                  <li><a href="../#apropos">À propos</a></li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <!-- End Header Area -->
+
+        
+
+        <!-- start banner Area -->
+        <section class="banner-areax relative mb-5" id="home">
+          <div class="overlay overlay-bg"></div>
+          <div class="container">
+            <div
+              class="row fullscreen d-flex align-items-center justify-content-center"
+            >
+              <div class="banner-content col-lg-10">
+                <h5 class="text-uppercase" id="greenlantern">
+                <i class="far fa-comments fa-4x" ></i>
+                </h5>
+                <h1 >Contact</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- End banner Area -->
+
+
+        <!-- Start CLIENTS Area -->
+        <div id="">
+          <section class="services-area pb-100 pt-100 mt-auto">
+              <div class="container">
+                <div class="row">
+                  <div class="col-lg-4" >
+                    <div id="bureau">
+                      <div class="single-service">
+                          <i class="fa fa-mobile-alt fa-4x" id=mobileIcon ></i>
+                          <h1 class="mb-10 text-dark" >Téléphone</h1>
+                        <h5><b>
+                          +212 69 000 9179 </b>
+                        </h5>
+                      </div>
+                    </div>    
+                    <a href="tel:+212690009179" id="mobile">
+                      <div class="single-service">
+                          <i class="fa fa-mobile-alt fa-4x" id=mobileIcon ></i>
+                          <h1 class="mb-10 text-dark" >Téléphone</h1>
+                        <h5><b>
+                          +212 69 000 9179 </b>
+                        </h5>
+                      </div>
+                    </a>    
+                  </div>
+                  <div class="col-lg-4">
+                  <a href="https://www.linkedin.com/in/oussama-hraich-23b84b164" target=_blank title="LinkedIn" >
+                    <div class="single-service" >
+                        <i class="fab fa-linkedin-in fa-4x" id=inin ></i>
+                        <h1 class="mb-10 text-dark">LinkedIn</h1>
+                        <h5 id=linktext>/oussama-hraich-23b84b164/</h5>
+
+                    </div>    
+                  </div>
+                  <div class="col-lg-4">
+                  <a href="mailto:hraich.oussama@gmail.com"  title="Mail" class="nohref text-dark">
+                    <div class="single-service">
+                        <i class="far fa-envelope fa-4x" id=gma></i>
+                        <h1 class="mb-10 text-dark">Mail</h1>
+                        <h5 id=likemail>Hraich.oussama@gmail.com</h5>
+                    </div>    
+                </a>
+                  </div>
+                </div>
+              </div>
+            </section>
+        </div>
+                  
+        <!-- End CLIENTS Area -->
+
+        <!-- START GO TO TOP  -->
+        <a href="javascript:" id="return-to-top">
+          <i class="fa fa-chevron-up"></i>
+        </a>
+        <!-- END GO TO TOP -->
+      </div>
+      <!-- END PORTFOLIO WRAP -->
+
+     <!-- start footer Area -->
+
+     <!-- start footer Area -->
+
+     <section id="footer">
+            <div class="container">
+  
+              <div class="row  text-xs-center text-center " id=bureau>
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                  <h5>Navigation</h5>
+                  <ul class="list-unstyled quick-links ">
+                    <li><a href="../#home"><i class="fa fa-angle-double-right"></i>Acceuil</a></li>
+                    <li><a href="../travail/"><i class="fa fa-angle-double-right"></i>Portfolio</a></li>
+                    <li><a href="../#apropos"><i class="fa fa-angle-double-right"></i>À propos</a></li>
+                    <li><a href="../contact"><i class="fa fa-angle-double-right"></i>Contact</a></li>
+                  </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                  <h5>Informations</h5>
+                  <ul class="list-unstyled quick-links">
+                    <li><a href="javascript:void();"><i class="fa fa-phone"></i>+212 6 90 00 91 79 </a></li>
+                    <li><a href="mailto:hraich.oussama@gmail.com"><i class="far fa-envelope"></i>Hraich.oussama@gmail.com</a></li>
+                  </ul>
+                </div>          
+              </div>
+  
+              <div class="row  text-xs-center text-center " id=mobile>
+                <div class="col-xs-12 col-sm-6 col-md-6">
+                  <h5>Navigation</h5>
+                  <ul class="list-unstyled quick-links ">
+                    <li><a href="../#home"><i class="fa fa-angle-double-right"></i>Acceuil</a></li>
+                    <li><a href="../travail"><i class="fa fa-angle-double-right"></i>Portfolio</a></li>
+                    <li><a href="../#apropos"><i class="fa fa-angle-double-right"></i>À propos</a></li>
+                    <li><a href="../contact/"><i class="fa fa-angle-double-right"></i>Contact</a></li>
+                  </ul>
+                </div>
+                
+                <div class="col-xs-12 col-sm-6 col-md-6" >
+                  <h5 class="mt-5">Informations</h5>
+                  <ul class="list-unstyled quick-links">
+                    <li><a href="tel:+212690009179"><i class="fa fa-phone"></i>+212 6 90 00 91 79 </a></li>
+                    <li><a href="mailto:hraich.oussama@gmail.com"><i class="far fa-envelope"></i>Hraich.oussama@gmail.com</a></li>
+                  </ul>
+                </div>
+               
+              </div>
+  
+              <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-3">
+                  <ul class="list-unstyled list-inline social text-center">
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/in/oussama-hraich-23b84b164/" target="_blank"><i class="fab fa-linkedin-in"></i></a></li>
+                    <li class="list-inline-item"><a href="mailto:hraich.oussama@gmail.com" target="_blank"><i class="fa fa-envelope"></i></a></li>
+                  </ul>
+                </div>
+                </hr>
+              </div>	
+  
+              <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+                  <p class="h6 text-white">&copy Hraich Oussama.</p>
+                </div>
+                </hr>
+              </div>	
+            </div>
+          </section>
+        <!-- end footer Area -->
+    </div>
+    <!-- END UNIVERSE -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="../js/jquery.sticky.js"></script>
+    <script src="../js/main.js"></script>
+    <script>
+
+        // ===== Scroll to Top ====
+        $(window).scroll(function() {
+          if ($(this).scrollTop() >= 50) {
+            // If page is scrolled more than 50px
+            $("#return-to-top").fadeIn(200); // Fade in the arrow
+          } else {
+            $("#return-to-top").fadeOut(200); // Else fade out the arrow
+          }
+        });
+        $("#return-to-top").click(function() {
+          // When arrow is clicked
+          $("body,html").animate(
+            {
+              scrollTop: 0 // Scroll to top of body
+            },
+            500
+          );
+        });
+        // end scroll to top 
+        
+    </script>
+  </body>
+</html>
